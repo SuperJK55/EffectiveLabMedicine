35a90bcca37bae2c67105832b93b76f5c094a273
SuperJK55 <stepan.kolenkin@mail.ru>
2023-11-15 20:34:47 +0600

    Initial Commit

---
+++ EffectiveLabMedicine/EffectiveLabMedicineApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveLabMedicineApp.swift
+//  EffectiveLabMedicine
+//
+//  Created by Степан Коленкин on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveLabMedicineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
