ace26fc371b1961e29de721a28e9263288894541
SuperJK55 <stepan.kolenkin@mail.ru>
2023-11-15 22:11:00 +0600

    Added DoctorTab

---
+++ EffectiveLabMedicine/Views/DoctorTab.swift
@@ -0,0 +1,18 @@
+//
+//  DoctorTab.swift
+//  EffectiveLabMedicine
+//
+//  Created by Степан Коленкин on 15.11.2023.
+//
+
+import SwiftUI
+
+struct DoctorTab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DoctorTab()
+}
